cab69cc2f7c3e711ff0509b99b9e3fe81c1a64c8
Simon Schmeinck <simon.schmeinck@smail.inf.h-brs.de>
2021-08-16 02:10:08 +0200

    Added Charts

--- src/Components/PairView.tsx
+++ src/Components/PairView.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Line, defaults } from 'react-chartjs-2';
 
 interface Currency{
     pair: string;
@@ -14,13 +15,150 @@ interface PairViewProps extends Currency{
 export const PairView: React.FC<PairViewProps> = (props) => {
     let firstCur = props.pair.split("/")[0];
     let secondCur = props.pair.split("/")[1];
+    const[chart, setChart] = useState(<></>);
+    const [showChart, setShowChart] = useState(false)
     return(
         <div className="PairView">
-            <p className="InfoView"><b>{firstCur}</b>: {props.price + secondCur}</p>
-            <button className="DelButton" title="Delete" onClick={() => {
-                props.setCurrencyList(props.currencyList.filter(currency => currency.pair !== props.pair));
-                props.unsubscribe(props.pair);
-            }}/>
+            <div className="PairContainer">
+                <button className="GraphButton" onClick={async() => drawChart(setChart, props.pair, "day", showChart, setShowChart)}></button>
+                <p className="InfoView"><b>{firstCur}</b>: {props.price + secondCur}</p>
+                <button className="DelButton" title="Delete" onClick={() => {
+                    props.setCurrencyList(props.currencyList.filter(currency => currency.pair !== props.pair));
+                    props.unsubscribe(props.pair);
+                }}/>
+            </div>
+            {showChart ? chart : null}
         </div>
     );
+}
+
+const drawChart = async(setChart: (el: JSX.Element) => void, pair: string, mode: string, showChart: boolean, setShowChart: (showChart: boolean) => void) => {
+
+    setShowChart(!showChart);
+
+    if(showChart) return;
+
+    let time = (await getChartData(pair, mode)).time;
+    let price = (await getChartData(pair, mode)).price;
+    let crypto = pair.split("/")[0];
+    let fiat = pair.split("/")[1];
+
+    let dat = {labels: time,
+      datasets: [{
+        label: `${crypto}/${fiat}`,
+        borderColor: "white",
+        data: price,
+        fill: true,
+        pointRadius: 0
+      }],
+    };
+
+    let opt = {
+      legend: {
+        labels: {
+          fontColor: "#ffffff",
+        }
+      },
+      tension: 0.5
+    };
+
+    defaults.color = "#ffffff";
+
+    let x = <Line data={dat} options={opt}/>;
+
+    setChart(x);
+}
+
+const getCur = async (pair: string) => {
+    let cryptoCur = pair.split("/")[0].toLowerCase();
+
+    let res = await fetch("https://api.coingecko.com/api/v3/coins/list");
+    if(res.status >= 400 && res.status < 600){
+        alert("Oh no! This currency seems to not exist!");
+        return "Error";
+    }
+    let jsonList = await res.json();
+    
+    for(let x of jsonList){
+        if((await x.symbol === cryptoCur) && (!JSON.stringify(await x.id).includes("binance"))){
+            return x.id;
+        }
+    }
+
+    return "Error";
+}
+
+const getChartData = async(pair: string, mode: string) => {
+
+    let crypto = await getCur(pair);
+    let fiat = pair.split("/")[1].toLowerCase();
+    let days = "0";
+    let interval = "";
+  
+    switch (mode) {
+      case "all":
+        days="max";
+        interval="";
+        break;
+      case "year":
+        days="365";
+        interval="weekly";
+        break;
+      case "month":
+        days = "30";
+        interval="daily";
+        break;
+      case "week":
+        days = "7";
+        interval="daily";
+        break;
+      case "day":
+        days = "1";
+        interval="hourly";
+        break;
+      case "hour":
+        days = "1";
+        interval="minutely";
+        break;
+      default:
+        break;
+    }
+    let reqUrl = `https://api.coingecko.com/api/v3/coins/${crypto}/market_chart?vs_currency=${fiat}&days=${days}&interval=${interval}`;
+
+    let res = await fetch(reqUrl);
+    if(res.status >= 400 && res.status < 600){
+        alert("Oh no! This currency seems to not exist!");
+        return {time: [""], price: []};
+    }
+    let json = await res.json();
+    let arr = await json.prices;
+    let time = [];
+    let price = [];
+    if(mode !== 'hour'){
+      for(let entry of arr){
+        let currentTime = await entry[0];
+        let date = new Date(await currentTime);
+        let dateString = `${date}`;
+        if(interval === 'hourly'){
+          dateString = dateString.substr(dateString.length - 48, 5);
+        }else{
+          dateString = date.toUTCString();
+          dateString = dateString.substr(5,dateString.length-17);
+        }
+        time.push(dateString);
+        price.push(await entry[1]);
+      }
+    }else{
+      for(let i = await arr.length-15; i< await arr.length; i++){
+        let entry = await arr[i];
+        let currentTime = await entry[0];
+        let date = new Date(await currentTime);
+        let dateString = `${date}`;
+        dateString = dateString.substr(dateString.length-48, 5);
+        time.push(dateString);
+        price.push(await entry[1]);
+      }
+    }
+
+    return {time: time, price: price};
 }
