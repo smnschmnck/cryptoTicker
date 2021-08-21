6e84ffa499616c371de0c942b0b084f0a2afda23
Simon Schmeinck <simon.schmeinck@smail.inf.h-brs.de>
2021-08-21 17:00:19 +0200

    Download.sh auto replace XBT w BTC and XDG w DOGE

--- Download.sh
+++ Download.sh
@@ -11,27 +11,33 @@ echo -n "export const assetPairs = [" >> AssetPairs.tsx
 
 for((i=0; i<${#array[@]}; i++))
     do
-       x=$(echo ${array[$i]} | sed s/wsname\":\"//)
-       x=$(echo $x | sed 's/\\//g')
-       x="\"$x\""
-       if [ "$i" -ne "$((${#array[@]}-1))" ]; then
-        echo -n $x, >> AssetPairs.tsx
+        x=$(echo ${array[$i]} | sed s/wsname\":\"//)
+        x=$(echo $x | sed 's/\\//g')
+        x="\"$x\""
+        if [[ $x =~ "XBT" ]]; then
+            x="${x/XBT/BTC}"   
+        fi
+        if [[ $x =~ "XDG" ]]; then
+            x="${x/XDG/DOGE}"
+        fi
+        if [ "$i" -ne "$((${#array[@]}-1))" ]; then
+            echo -n $x, >> AssetPairs.tsx
         fi
         if [ "$i" -eq "$((${#array[@]}-1))" ]; then
-        echo -n $x >> AssetPairs.tsx
+            echo -n $x >> AssetPairs.tsx
         fi
     done
 
-echo -n "].sort();" >> AssetPairs.tsx
+ echo -n "].sort();" >> AssetPairs.tsx
 
-cd ..
+ cd ..
 
-rm -R ../crypto-ticker-prod/build
+ rm -R ../crypto-ticker-prod/build
 
-npm run build
+ npm run build
 
-cp -R ./build ../crypto-ticker-prod
+ cp -R ./build ../crypto-ticker-prod
 
-cd ../crypto-ticker-prod
+ cd ../crypto-ticker-prod
 
-npm start
+ npm start
